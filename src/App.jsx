9e812c9809a3ede9c0cec9d9c29cda131b98c186
Balaji T S <Balaji.T.S1@ibm.com>
2024-06-13 17:55:25 +0530

    dev changes

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ function App() {
         <Route path="/register" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/forgot-password?/:mode/:oobCode" element={<ForgotPassword />} />
-        {/* /?mode=resetPassword&oobCode=1V96GR_oTpYqAG1qKNONwvqhn83hbDBBCP3wfhJ31ZsAAAGQETg4ng&apiKey=AIzaSyB6VUdwpwoITNUWLVkanmVB87PK9lUlBlg&lang=en */}
         {/* public routes ends */}
 
         {/* private routes starts */}
